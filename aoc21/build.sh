90e422b3c5fa905fccd704e3a37e2488e10e9c7d
nakst <>
2021-12-26 17:17:23 +0000

    update

--- aoc21/build.sh
+++ aoc21/build.sh
@@ -1,9 +1,9 @@
 cd aoc21
 gcc -o runner runner.cpp
-./runner "gcc -O0 -o"
-./runner "gcc -O1 -o"
-./runner "gcc -O2 -o"
-./runner "gcc -O3 -o"
+./runner "g++ -O0 -o"
+./runner "g++ -O1 -o"
+./runner "g++ -O2 -o"
+./runner "g++ -O3 -o"
 ./runner "clang -O0 -o"
 ./runner "clang -O1 -o"
 ./runner "clang -O2 -o"
